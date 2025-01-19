29093adfa2d361f90a9b28fbd5fb0ed573d9e7a9
BoneDeath <mhasannudin300@gmail.com>
2025-01-19 14:06:48 +0700

    camera problem

--- socketHandlers/index.js
+++ socketHandlers/index.js
@@ -1,31 +1,37 @@
-const players =[];
+const players = [];
+let world = { width: 2000, height: 2000 };
+let canvas = {};
+let camera = { x: 0, y: 0, width: canvas.width, height: canvas.height };
 
-function updatePlayerPosition(id, move) {
-  const me = players.find(player =>player.id == id);
-  if (me) {
-    switch (move) {
-      case "left":
-        me.x -= 1;
-        break;
-      case "up":
-        me.y -= 1;
-        break;
-      case "right":
-        me.x += 1;
-        break;
-      case "down":
-        me.y += 1;
-        break;
-    }
-  }
+function updateCamera(me) {
+  camera.x = Math.max(
+    0,
+    Math.min(world.width - camera.width, me.x - camera.width / 2)
+  );
+  camera.y = Math.max(
+    0,
+    Math.min(world.height - camera.height, me.y - camera.height / 2)
+  );
 }
 
 module.exports = (io) => {
+  myio = io;
   io.on("connection", (socket) => {
-    io.emit("server", "connected to server");
+    io.emit("server", "connected online " + players.length);
+    io.emit("world", world);
+    io.emit("camera", camera);
 
     socket.on("join", (id) => {
-      players.push({ id, x: 0, y: 0 });
+      const me = players.find((player) => player.id == id);
+      if (!me) {
+        players.push({ id, x: 0, y: 0, width: 50, height: 50, speed: 5 });
+      }
+    });
+
+    socket.on("canvas", (playerCanvas) => {
+      canvas = playerCanvas;
+      camera.width = playerCanvas.width;
+      camera.height = playerCanvas.width;
     });
 
     socket.on("update", () => {
@@ -35,7 +41,32 @@ module.exports = (io) => {
     socket.on("move", (data) => {
       if (data && data.id && data.move) {
         updatePlayerPosition(data.id, data.move);
+        myio.emit("camera", camera);
       }
     });
   });
 };
+
+function updatePlayerPosition(id, move) {
+  const me = players.find((player) => player.id == id);
+
+  if (me) {
+    switch (move) {
+      case "up":
+        me.y = Math.max(0, me.y - me.speed);
+        break;
+      case "down":
+        me.y = Math.min(world.height - me.height, me.y + me.speed);
+        break;
+      case "left":
+        me.x = Math.max(0, me.x - me.speed);
+        break;
+      case "right":
+        me.x = Math.min(world.width - me.width, me.x + me.speed);
+        break;
+    }
+    updateCamera(me);
+  } else {
+    console.log("your character not in this world");
+  }
+}

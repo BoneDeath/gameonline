3b074fa6cb7d19c288929cce5adbff7c3df210e0
BoneDeath <mhasannudin300@gmail.com>
2025-01-19 14:48:17 +0700

    added camera

--- socketHandlers/index.js
+++ socketHandlers/index.js
@@ -1,37 +1,18 @@
 const players = [];
 let world = { width: 2000, height: 2000 };
-let canvas = {};
-let camera = { x: 0, y: 0, width: canvas.width, height: canvas.height };
 
-function updateCamera(me) {
-  camera.x = Math.max(
-    0,
-    Math.min(world.width - camera.width, me.x - camera.width / 2)
-  );
-  camera.y = Math.max(
-    0,
-    Math.min(world.height - camera.height, me.y - camera.height / 2)
-  );
-}
 
 module.exports = (io) => {
   myio = io;
   io.on("connection", (socket) => {
     io.emit("server", "connected online " + players.length);
     io.emit("world", world);
-    io.emit("camera", camera);
 
     socket.on("join", (id) => {
       const me = players.find((player) => player.id == id);
       if (!me) {
         players.push({ id, x: 0, y: 0, width: 50, height: 50, speed: 5 });
       }
-    });
-
-    socket.on("canvas", (playerCanvas) => {
-      canvas = playerCanvas;
-      camera.width = playerCanvas.width;
-      camera.height = playerCanvas.width;
     });
 
     socket.on("update", () => {
@@ -41,7 +22,6 @@ module.exports = (io) => {
     socket.on("move", (data) => {
       if (data && data.id && data.move) {
         updatePlayerPosition(data.id, data.move);
-        myio.emit("camera", camera);
       }
     });
   });
@@ -65,7 +45,6 @@ function updatePlayerPosition(id, move) {
         me.x = Math.min(world.width - me.width, me.x + me.speed);
         break;
     }
-    updateCamera(me);
   } else {
     console.log("your character not in this world");
   }
